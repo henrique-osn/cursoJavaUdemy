351e7bbc7c94fee7e9897df44e984e087f17b358
Henrique Nascimento <henrique.osn@gmail.com>
2024-01-21 11:48:39 -0300

    Exercício 002 concluído

--- src/exercises/Exercise002.java
+++ src/exercises/Exercise002.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Exercise002 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
