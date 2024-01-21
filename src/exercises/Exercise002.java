6daeb1fc02863fda4db3e5fa88f883db2cd3201a
Henrique Nascimento <henrique.osn@gmail.com>
2024-01-21 11:47:58 -0300

    Exercício 002 concluído

---
+++ src/exercises/Exercise002.java
@@ -0,0 +1,22 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Exercise002 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int numero1 = sc.nextInt();
+        int numero2 = sc.nextInt();
+
+        int soma = numero1 + numero2;
+
+        System.out.println("SOMA = " + soma);
+
+        sc.close();
+
+    }
+
+}
