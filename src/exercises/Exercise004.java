6f84af5338d7e8c1e391abffad408b1a4819ada2
Henrique Nascimento <henrique.osn@gmail.com>
2024-01-22 19:43:07 -0300

    Exercício 004 concluído

---
+++ src/exercises/Exercise004.java
@@ -0,0 +1,24 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Exercise004 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+
+        int DIFERENCA = A * B - C * D;
+
+        System.out.println("DIFERENCA = " + DIFERENCA);
+
+        sc.close();
+
+    }
+
+}
