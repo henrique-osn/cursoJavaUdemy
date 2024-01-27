0b24bb8ef3dfc795ec4eae6e7b686afb4d8a1dd1
Henrique Nascimento <henrique.osn@gmail.com>
2024-01-27 17:43:33 -0300

    Exercício 018 concluído

---
+++ src/exercises/Exercise018.java
@@ -0,0 +1,44 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Exercise018 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
+
+        int tipo = sc.nextInt();
+
+        while (tipo != 4) {
+            switch (tipo) {
+                case 1:
+                    alcool += 1;
+                    break;
+                case 2:
+                    gasolina += 1;
+                    break;
+                case 3:
+                    diesel += 1;
+                    break;
+                default:
+                    System.out.println("Opção Invalida!");
+            }
+
+            tipo = sc.nextInt();
+        }
+
+        System.out.println("MUITO OBRIGADO");
+        System.out.println("Álcool: " + alcool);
+        System.out.println("Gasolina: " + gasolina);
+        System.out.println("Diesel: " + diesel);
+
+        sc.close();
+
+    }
+
+}
